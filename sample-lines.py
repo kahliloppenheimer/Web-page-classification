42f74b62ed67368d6f1df067ed1ac178a9777921
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2015-11-30 23:10:22 -0500

    Initial commit

---
+++ sample-lines.py
@@ -0,0 +1,44 @@
+# Samples lines randomly from a given file
+#
+# use:
+# python sample-lines.py input_file output_file num_lines
+from __future__ import print_function
+import numpy as np
+import codecs
+
+# Setup unicode encoding to work
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
+
+line_seps = [u'0x000A', u'0x000B', u'0x000C', u'0x000D', u'0x001C', u'0x001D', u'0x001E', u'0x0085', u'0x2028', u'0x2029']
+
+# Returns the number of lines of a given file
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+# Returns set of n line #s randomly selected from the file
+def getRandLineNums(file, n):
+    totalLines = np.arange(file_len(input))
+    linesToPull = np.random.choice(totalLines, SAMPLE_SIZE, False)
+    return set(linesToPull)
+
+# Grab command line args (input, output, size)
+input = sys.argv[1]
+output = sys.argv[2]
+SAMPLE_SIZE = int(sys.argv[3])
+
+# Pull out SAMPLE_SIZE random lines from the input file and print to putput file
+lineNums = getRandLineNums(input, SAMPLE_SIZE)
+with open(output, 'w') as output:
+    with open(input) as input:
+        for i, l in enumerate(input):
+            if i in lineNums:
+                l = l.decode('unicode-escape')
+                # Handles weird occasional unicode newlines that are created in parsing
+                new = ''.join(l.splitlines()) + '\n'
+                output.write(new)
