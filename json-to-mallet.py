44bb4bab7c87ec2bf53104832d41a99b81ff2583
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2015-12-10 20:25:09 -0500

    Changed JSON -> Mallet script to output directory format

--- json-to-mallet.py
+++ json-to-mallet.py
@@ -9,45 +9,48 @@ import os
 # Prints out the given json object to the given output stream
 # with the features as obj[feature_field]. Returns True if
 # conversion from JSON to mallet input was succesful, false otherwise
-def printObj(out, obj, feature_field):
-    name = obj['url']
+def printObj(dir, obj, feature_field):
+    # Since we're going to use these as file names, we can't have slashes
+    name = obj['url'].replace('/', '')
     label = obj['topic'].split('/')
     # Weird cases where there is no label like top/*
     if(len(label) < 2):
         return False
     label = label[1]
     features = obj[feature_field]
+    instance_path = os.path.join(dir, label)
     if (name and label and features):
-        out.write(name + '\t' + label + '\t' + features + '\n')
+        if not os.path.isdir(instance_path):
+            os.makedirs(instance_path)
+        with open(os.path.join(instance_path, name), 'w') as f:
+            f.write(features)
         return True
     else:
         return False
 
 input = sys.argv[1]
-output = sys.argv[2]
+output_dir = sys.argv[2]
 feature_field = sys.argv[3]
 
 sCount = 0
 total = 0
 
 if (os.path.isdir(input)):
-    with open(output, 'w') as out:
-        for fName in os.listdir(input):
-            with open(os.path.join(input, fName)) as f:
-                for l in f:
-                    total += 1
-                    obj = json.loads(l)
-                    if(printObj(out, obj, feature_field)):
-                        sCount += 1
-
-else:
-    with open(output, 'w') as out:
-        with open(input) as f:
+    for fName in os.listdir(input):
+        with open(os.path.join(input, fName)) as f:
             for l in f:
                 total += 1
-                print(total)
                 obj = json.loads(l)
-                if(printObj(out, obj, feature_field)):
+                if(printObj(output_dir, obj, feature_field)):
                     sCount += 1
+
+else:
+    with open(input) as f:
+        for l in f:
+            total += 1
+            print(total)
+            obj = json.loads(l)
+            if(printObj(output_dir, obj, feature_field)):
+                sCount += 1
 
 print(100 * float(sCount) / total)
