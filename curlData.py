59a0d3d31e155ab5b19b64dad9f804ad3b1358f0
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2015-12-11 12:27:11 -0500

    Merged curlData and json-to-mallet, so you only need to use curlData

--- curlData.py
+++ curlData.py
@@ -31,44 +31,60 @@ def cleanHtml(html):
     text = ' '.join(chunk for chunk in chunks if chunk)
     return text
 
+
+# Prints out the given json object to the output directory
+# with the features as obj[feature_field], file name as obj['url'],
+# and subdirectory (label) as obj['topic']. Returns True if
+# conversion from JSON to mallet input was succesful, false otherwise
+def printObj(dir, obj, feature_field):
+    # Since we're going to use these as file names, we can't have slashes
+    name = obj['url'].replace('/', '')
+    label = obj['topic'].split('/')
+    # Weird cases where there is no label like top/*
+    if(len(label) < 2):
+        return False
+    label = label[1]
+    features = obj[feature_field]
+    instance_path = os.path.join(dir, label)
+    if (name and label and features):
+        if not os.path.isdir(instance_path):
+            os.makedirs(instance_path)
+        with open(os.path.join(instance_path, name), 'w') as f:
+            f.write(features.encode('utf8'))
+        return True
+    else:
+        return False
+
 input = sys.argv[1]
 output_dir = sys.argv[2]
 
 numLines = getNumLines(input)
 numErrors = 0
 
-# Create output directory if it doesn't exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
 with open (input) as f:
     for i, line in enumerate(f):
-        next_file = os.path.join(output_dir, 'd' + str(i) + '.json')
-        with open(next_file, 'w') as out:
-            if ((i + 1) % 50 < .00001):
-                print 100 * float(i + 1) / numLines, '% done', 'nErrors:', numErrors, '(', 100 * float(numErrors) / i, ')%'
-            # First call to loads removes escape characters but does not turn
-            # into dict for strange reason
-            doc = json.loads(line)
-            if (type(doc) == type(u'foo')):
-                doc = json.loads(doc)
-            try:
-                html = unicode(urllib2.urlopen(doc['url'], timeout = 5).read(), errors='ignore')
-                origLength = len(html)
-                cleaned = cleanHtml(html)
-                doc['html'] = cleaned
-                json.dump(doc, out)
-                out.write('\n')
-                print 'line', i + 1, 'html length:', len(html), 'stripped length:', len(cleaned)
-            except socket.timeout, e:
-                numErrors += 1
-                print 'URL timeout:', doc['url']
-            except urllib2.URLError, e:
-                numErrors += 1
-                print 'URL timeout:', doc['url']
-            except urllib2.HTTPError, e:
-                numErrors += 1
-                print 'SKIPPED (HTTPError):', doc['url']
-            except Exception as e:
-                numErrors += 1
-                print 'other error'
+        if ((i + 1) % 50 < .00001):
+            print 100 * float(i + 1) / numLines, '% done', 'nErrors:', numErrors, '(', 100 * float(numErrors) / i, ')%'
+        # First call to loads removes escape characters but does not turn
+        # into dict for strange reason
+        doc = json.loads(line)
+        if (type(doc) == type(u'foo')):
+            doc = json.loads(doc)
+        try:
+            html = unicode(urllib2.urlopen(doc['url'], timeout = 5).read(), errors='ignore')
+            doc['html'] = cleanHtml(html)
+            printObj(os.path.join(output_dir, 'desc'), doc, 'd:Description')
+            printObj(os.path.join(output_dir, 'html'), doc, 'html')
+            print 'line', i + 1, '(', 100 * float(i) / numLines, '%)'
+        except socket.timeout, e:
+            numErrors += 1
+            print 'URL timeout:', doc['url']
+        except urllib2.URLError, e:
+            numErrors += 1
+            print 'URL timeout:', doc['url']
+        except urllib2.HTTPError, e:
+            numErrors += 1
+            print 'SKIPPED (HTTPError):', doc['url']
+        except Exception as e:
+            numErrors += 1
+            print e
