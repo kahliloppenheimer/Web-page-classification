6c41ad7085c410c8316c5b22d5f3d2131b4bcae4
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2015-12-13 01:09:34 -0500

    Added more classifiers to summary test

--- test-summary.sh
+++ test-summary.sh
@@ -2,4 +2,4 @@
 # Takes summary algorithm as input (i.e. luhn, edmundson, etc.)
 python prepData.py ../old/dmoz10.json raw/summary/${1} ${1}
 (cd mallet && bin/mallet import-dir --remove-stopwords --input ../raw/summary/${1}/html --output input/summary/${1}.mallet)
-(cd mallet && bin/mallet train-classifier --input input/summary/${1}.mallet --cross-validation 10 | tee output/summary/${1}.out)
+(cd mallet && bin/mallet train-classifier --input input/summary/${1}.mallet --cross-validation 10 --trainer NaiveBayes --trainer BalancedWinnow --trainer MaxEnt | tee output/summary/${1}.out)
